48d2386aa0929e0be85d4d67262c58528511d042
alex <alex.t@itua.com.ua>
2018-06-24 14:03:09 +0300

    adaptive sliders

--- src/components/others/slider/slider_news/slider_news.js
+++ src/components/others/slider/slider_news/slider_news.js
@@ -6,13 +6,31 @@ const sliderNewsSettings = {
     centeredSlides: true,
     initialSlide: 2,
     loop: true,
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
+    },
+    // Responsive breakpoints
+    breakpoints: {
+        // when window width is <= 576px
+        576: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+            centeredSlides: false,
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+            }
+        },
+        // when window width is <= 768px
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 30,
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+            }
+        }
     }
 };
 
